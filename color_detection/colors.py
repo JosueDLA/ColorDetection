920c3b63dcdca0b4e2d3d48ce7e627e68c9e90d6
JosueDLA <josuedlavs@gmail.com>
2020-09-22 09:31:16 -0600

    Initial Commit

---
+++ color_detection/colors.py
@@ -0,0 +1,39 @@
+BLACK = {
+    "lower": [],
+    "upper": []
+}
+
+BLUE = {
+    "lower": [],
+    "upper": []
+}
+
+GREEN = {
+    "lower": [],
+    "upper": []
+}
+
+ORANGE = {
+    "lower": [],
+    "upper": []
+}
+
+PURPLE = {
+    "lower": [],
+    "upper": []
+}
+
+RED = {
+    "lower": [],
+    "upper": []
+}
+
+YELLOW = {
+    "lower": [],
+    "upper": []
+}
+
+WHITE = {
+    "lower": [],
+    "upper": []
+}
