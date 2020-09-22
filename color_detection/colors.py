235ecb71e997562dad960901a35e2d6b8cdace4d
JosueDLA <josuedlavs@gmail.com>
2020-09-22 10:11:33 -0600

    Added colors threshold

--- color_detection/colors.py
+++ color_detection/colors.py
@@ -1,39 +1,39 @@
 BLACK = {
-    "lower": [],
-    "upper": []
+    "lower": [0, 0, 0],
+    "upper": [180, 255, 30]
 }
 
 BLUE = {
-    "lower": [],
-    "upper": []
+    "lower": [90, 60, 0],
+    "upper": [121, 255, 255]
 }
 
 GREEN = {
-    "lower": [],
-    "upper": []
+    "lower": [40, 70, 80],
+    "upper": [70, 255, 255]
 }
 
 ORANGE = {
-    "lower": [],
-    "upper": []
+    "lower": [10, 50, 120],
+    "upper": [20, 255, 255]
 }
 
 PURPLE = {
-    "lower": [],
-    "upper": []
+    "lower": [35, 60, 0],
+    "upper": [45, 255, 255]
 }
 
 RED = {
-    "lower": [],
-    "upper": []
+    "lower": [0, 50, 120],
+    "upper": [10, 255, 255]
 }
 
 YELLOW = {
-    "lower": [],
-    "upper": []
+    "lower": [25, 70, 120],
+    "upper": [30, 255, 255]
 }
 
 WHITE = {
-    "lower": [],
-    "upper": []
+    "lower": [0, 0, 200],
+    "upper": [180, 255, 255]
 }
